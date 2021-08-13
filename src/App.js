317bffd949d7dc731574120ee5e6734a5ce65ce9
demonicirfan <hashtag.irfan@gmail.com>
2021-08-13 19:41:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 
 
 function App() {
+  // tutorial --> https://www.youtube.com/watch?v=BS6blX035NM
   return (
     <>
     <Header/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import "./Header.css"
+import React from "react";
 
 function Header() {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div class="header">
+        <a href="#default" class="logo">
+          IRFAN
+        </a>
+        <div class="header-right">
+          <a class="active" href="#home">
+            Home
+          </a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
